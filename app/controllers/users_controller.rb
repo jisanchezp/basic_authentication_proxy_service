53437665f6b18e4b72885b6e7e3ddd4830c4199a
jisanchezp <juanisanchez@outlook.com>
2018-01-18 20:25:09 -0300

    Implement basic CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,39 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path #@user
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find_by_id(params[:id])    
+    if @user.update_attributes(user_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-  end  
+    User.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :image, :image_file)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118175618) do
+ActiveRecord::Schema.define(version: 20180118223737) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
