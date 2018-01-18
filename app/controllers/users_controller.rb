3413211f9b4ce279b7b98eb7a028330e8bb84f2b
jisanchezp <juanisanchez@outlook.com>
2018-01-18 19:31:55 -0300

    Base User CRUD wireframes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @users = !User.all.nil? ? User.all : {}
   end
 
   def new
+    @user = User.new
   end
 
   def create
+  end
+
+  def show
   end
 
   def update
   end
 
   def destroy
-  end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   get 'users/new'
-  get 'users/create'
-  get 'users/update'
+  post 'users/create'
+  post 'users/update'
   get 'users/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
